bd00107a243d1d35a7dc05b3a487f48917858046
pal-kamlesh <kamleshiit5@gmail.com>
2024-12-18 17:27:28 +0530

    modified script

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App";
 import { DataProvider } from "./context/data_context";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
   <React.StrictMode>
     <DataProvider>
       <App />
     </DataProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
